bfb651a97f9d5bef685a03756aef10a3314e2243
ragingwind <ragingwind@gmail.com>
2001-06-13 00:00:00 +0900

    Migrate 2015

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 'use strict';
 
-var LoopArray = function () {
-	this.cursor = -1;
-};
-
-// interit from array
-LoopArray.prototype = Array.prototype;
-
-// append items by array, object or regex as types
-LoopArray.prototype.append = function () {
-	let max = 0;
-	const args = arguments;
-	let iterator;
-
-	if (typeof args[0] === 'number') {
-		max = args[0];
-		iterator = args[1];
-	} else if (args[0] instanceof Array) {
-		max = args[0].length;
-		iterator = function (index) {
-			return args[0][index];
-		};
-	} else if (typeof args[0] === 'object') {
-		max = 1;
-		iterator = function () {
-			return args[0];
-		};
-	} else {
-		throw new Error('LoopArray got unknown arguments type');
+class LoopArray extends Array {
+	constructor() {
+		super();
+		this.cursor = -1;
 	}
 
-	for (var i = 0; i < max; ++i) {
-		this.push(iterator(i, this.length));
+	append() {
+		let max = 0;
+		const args = arguments;
+		let iterator;
+
+		if (typeof args[0] === 'number') {
+			max = args[0];
+			iterator = args[1];
+		} else if (args[0] instanceof Array) {
+			max = args[0].length;
+			iterator = function (index) {
+				return args[0][index];
+			};
+		} else if (typeof args[0] === 'object') {
+			max = 1;
+			iterator = function () {
+				return args[0];
+			};
+		} else {
+			throw new Error('LoopArray got unknown arguments type');
+		}
+
+		console.log('obj', this);
+		for (var i = 0; i < max; ++i) {
+			this.push(iterator(i, this.length));
+		}
+
+		return this.length;
 	}
 
-	return this.length;
-};
+	clear() {
+		this.splice(0, this.length);
+		this.cursor = -1;
+	}
 
-// clear all items.
-LoopArray.prototype.clear = function () {
-	this.splice(0, this.length);
-	this.cursor = -1;
-};
+	remove() {
+		this.remove.apply(this, arguments);
+		this.cursor = -1;
+		return this.length;
+	}
 
-LoopArray.prototype.remove = function () {
-	this.remove.apply(this, arguments);
-	this.cursor = -1;
-	return this.length;
-};
-
-// it will return next item.
-LoopArray.prototype.beng = function () {
-	this.cursor = (this.cursor + 1) % this.length;
-	return this[this.cursor];
-};
+	beng() {
+		this.cursor = (this.cursor + 1) % this.length;
+		return this[this.cursor];
+	}
+}
 
 function bengbenge(dns) {
 	if (!bengbenge.dns[dns]) {
